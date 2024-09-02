eed54b97ad8b101802306063508e9588f82899f5
sametyildirim314 <sametyldrmckrxwz@gmail.com>
2024-09-02 14:17:17 +0300

    imdb project has been created

---
+++ IMDB.py
@@ -0,0 +1,24 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+
+
+url="https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
+
+headers={
+    "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36"
+}
+
+html=requests.get(url,headers=headers).content
+
+ 
+soup=BeautifulSoup(html,"html.parser")
+
+data=soup.find("ul",{"class":"ipc-metadata-list"}).find_all("li",limit=10)
+
+
+for item in data:
+    movie_name=item.find("h3",{"class":"ipc-title__text"}).text
+    imdb=item.find("span",{"class":"ipc-rating-star"}).text
+ 
+    print(movie_name, imdb)
